4a4450c08d98fe5ca1d4609e9032530174d10952
Michał Wąsiński <micwasi2003@gmail.com>
2025-05-23 02:47:22 +0200

    Initial commit

---
+++ backend/src/test/java/pl/edu/streamfinder/StreamfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.streamfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
